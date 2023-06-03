399209d6c9643560dde81ebcbd6e02940f9743c8
Roger Miret <r.mir.min@gmail.com>
2023-06-03 12:49:12 +0200

    change img url

--- src/app/Home/home.component.ts
+++ src/app/Home/home.component.ts
@@ -7,10 +7,10 @@ import { Escena } from "../interfaces";
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  frasesH: Escena[] = [{ txt: "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial", img: "../../assets/img/0.jpg" },
-    { txt: "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.", img: "../../assets/img/1.jpg" },
-    { txt: "L'heroi va decidir travessar la porta que el portava a casa.", img: "../../assets/img/2.jpg" },
-    { txt: "Mentrestant, altres herois no van tenir tanta sort en l'elecció ...", img: "../../assets/img/3.jpg" }];
+  frasesH: Escena[] = [{ txt: "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial", img: "/ITAcademy-6-a-tale/assets/img/0.jpg" },
+    { txt: "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.", img: "/ITAcademy-6-a-tale/assets/img/1.jpg" },
+    { txt: "L'heroi va decidir travessar la porta que el portava a casa.", img: "/ITAcademy-6-a-tale/assets/img/2.jpg" },
+    { txt: "Mentrestant, altres herois no van tenir tanta sort en l'elecció ...", img: "/ITAcademy-6-a-tale/assets/img/3.jpg" }];
   
   bi: boolean = false;
 
